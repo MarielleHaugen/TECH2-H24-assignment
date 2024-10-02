6ced4e778881f7ddfebba389fc94f3e38e4a548e
Marielle Jeanette Vestbøstad Haugen <mariellevestbostad@Marielles-MacBook-Air.local>
2024-10-02 12:43:44 +0200

    finished

--- part_A.py
+++ part_A.py
@@ -25,16 +25,16 @@ def std_loops(x):
     sd : float
         Standard deviation of the list of numbers.
     """
-    N = len(x) #length of list
+    N = len(x) # Length of list
 
-    # accumulate the sum of all numbers in the list
+    # Accumulate the sum of all numbers in the list
     total_sum = 0  
     total_square_sum = 0 
 
     # Loop through each number in the list
-    for num in x: #iterate over each element in x (num_lst) and assign it to the variable num.
-       total_sum += num #sum of the numbers
-       total_square_sum += num**2 #sum of the squares of the numbers
+    for num in x: # Iterate over each element in x (num_lst) and assign it to the variable num
+       total_sum += num # Sum of the numbers
+       total_square_sum += num**2 # Sum of the squares of the numbers
 
     # Compute the mean
     mean = total_sum / N 
@@ -74,7 +74,7 @@ def std_builtin(x):
     mean = sum(x) / N
 
     # Compute the mean of squares
-    mean_of_squares = sum(map(lambda num: num**2, x)) / N #map() function applies the function to each element of the list, and lambda num: num**2 takes the argument (num) and returns its square(num**2)
+    mean_of_squares = sum(map(lambda num: num**2, x)) / N # map() function applies the function to each element of the list, and lambda num: num**2 takes the argument (num) and returns its square(num**2)
 
     # Compute the variance
     variance = mean_of_squares - mean**2
@@ -91,6 +91,6 @@ print("Standard Deviation: ", std_dev)
 def std_numpy(x): 
     return np.std(x)
 
-# Call the function and print the Standard Deviation
+# Call the function and print the standard deviation
 std_dev = std_numpy(num_lst)
 print("Standard Deviation: ", std_dev)
